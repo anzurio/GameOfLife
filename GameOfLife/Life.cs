0f28648c68d790c7f93bf3a7524ef172ea562b7d
Mario Parra <anzurio@gmail.com>
2022-02-10 18:52:42 -0600

    Add Life

--- GameOfLife/Life.cs
+++ GameOfLife/Life.cs
@@ -6,8 +6,27 @@
         private readonly List<ICell> cells = new();
 
         public Life(int columns, int rows)
-        { 
+        {
             Cells = new ICell[columns, rows];
+        }
+
+        public void Tick()
+        {
+            ForEachCell(cell => cell.CalculateNextTick());
+            ForEachCell(cell => cell.Tick());
+        }
+
+        public void ForEachCell(Action<ICell> predicate)
+        {
+            if (cells.Count == 0)
+            {
+                foreach (var cell in Cells)
+                {
+                    cells.Add(cell);
+                }
+            }
+
+            cells.ForEach(predicate);
         }
 
     }
